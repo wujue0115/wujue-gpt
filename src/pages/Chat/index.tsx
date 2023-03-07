d21f94c5043d537d61b14745dbc37bee72086f5b
鄭子輿 <wujue@zhengziyude-MacBook-Pro.local>
2023-03-08 00:18:57 +0800

    feat: design chat page style

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom";
+import "./index.css";
+
 const Chat = () => {
   return (
-    <div>
-      <h1>Chat</h1>
+    <div className="chat flex flex-center-center">
+      <div className="wrapper flex flex-column">
+        <Link to="/" className="back btn font-size-4">Back</Link>
+        <div className="chat-pane">
+        </div>
+        <div className="input-pane flex">
+          <textarea className="user-input"/>
+          <div className="btn font-size-4">Enter</div>
+        </div>
+      </div>
     </div>
   )
 }
