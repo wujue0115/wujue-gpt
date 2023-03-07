fa351b679ae0fd9fb83a65ef7a3fe43c369b4558
鄭子輿 <wujue@zhengziyude-MacBook-Pro.local>
2023-03-08 00:18:26 +0800

    feat: design root page style

--- src/pages/Root/index.tsx
+++ src/pages/Root/index.tsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+import "./index.css";
+
 const Root = () => {
   return (
-    <div>
-      <h1>Root</h1>
+    <div className="root flex flex-column flex-center-center">
+      <h1 className="title">Welcome to the Wujue GPT</h1>
+      <div>
+        <Link className="link btn font-size-2" to="/chat">Start</Link>
+      </div>
     </div>
   )
 }
