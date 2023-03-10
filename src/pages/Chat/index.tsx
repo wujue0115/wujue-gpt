d43cc0fd9507befd837932c1b92db26d74df0888
鄭子輿 <wujue@zhengziyude-MacBook-Pro.local>
2023-03-10 12:29:01 +0800

    feat: adjust chat style to enhance user experience

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { Link } from "react-router-dom";
 import { MessageType } from "../../types/message.types";
 import chatApi from "../../api/chat-api";
@@ -7,15 +7,16 @@ import "./index.css";
 function parseChunkToContents(chunk: Uint8Array): string[] {
   const chunkData: string = new TextDecoder().decode(chunk);
   const dataArray: string[] = chunkData
-                      .trim()
-                      // Since the format of chunkData as follows: "data: {...} \n data: {...} ...",
-                      // use the split method to separate each data JSON string object
-                      .split("data:") 
-                      // Remove the content of the array at index 0 because it is ""
-                      .filter(str => str !== "")
-                      // Each JSON string object containing data may have leading and trailing whitespace and newline characters, 
-                      // which need to be stripped or cleaned. 
-                      .map(str => str.trim());
+    .trim()
+    // Since the format of chunkData as follows: "data: {...} \n data: {...} ...",
+    // use the split method to separate each data JSON string object
+    .split("data:") 
+    // Remove the content of the array at index 0 because it is ""
+    .filter(str => str !== "")
+    // Each JSON string object containing data may have leading and trailing whitespace and newline characters, 
+    // which need to be stripped or cleaned. 
+    .map(str => str.trim());
+
   const contents = dataArray.map(data => {
     const jsonData = data.includes("[DONE]") ? null : JSON.parse(data);
     return jsonData ? jsonData.choices[0].delta.content || "" : "";
@@ -26,6 +27,7 @@ function parseChunkToContents(chunk: Uint8Array): string[] {
 const Chat = () => {
   const [content, setContent] = useState<string>("");
   const [messages, setMessages] = useState<MessageType[]>([]);
+  const chatPaneRef = useRef<HTMLInputElement>(null);;
 
   const submitMessages = async () => {
     if (!content) {
@@ -48,13 +50,18 @@ const Chat = () => {
         const lastMessage = newMessages[newMessages.length - 1]; 
         lastMessage.content += contents.join(""); 
         setMessages([...newMessages.slice(0, -1), lastMessage]);
+        if (chatPaneRef.current) {
+          chatPaneRef.current.scrollTop = chatPaneRef.current.scrollHeight;
+        }
       }
     } catch (error) {
       console.error(error)
     }
-  }
+  } 
 
   const handleContentChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    event.target.style.height = "0px";
+    event.target.style.height = event.target.scrollHeight + "px";
     setContent(event.target.value);
   }
 
@@ -62,22 +69,33 @@ const Chat = () => {
     <div className="chat flex flex-center-center">
       <div className="wrapper flex flex-column">
         <Link to="/" className="back btn font-size-4">Back</Link>
-        <div className="chat-pane">
-          {messages.map((message, index) => {
-            return (
-              <div 
-                key={message.role + index}
-                className={message.role === "user" ? "user-pane" : "assistant-pane"}
-              >
-                <h3>{message.role}</h3>
-                <p>{message.content}</p>
-              </div>
-            )
+        <div className="chat-pane" ref={chatPaneRef}>
+          {messages.map((message, mIndex) => {
+            return message.content
+              .split("\n")
+              .filter(str => str !== "")
+              .map((content, cIndex) => {
+                return (
+                  <div 
+                    key={message.role + mIndex + cIndex}
+                    className={"flex  " + (message.role === "user" ? "user-pane" : "assistant-pane")}
+                  >
+                    <div className="content-pane">
+                      {/* <h3>{message.role}</h3> */}
+                      <p>{content}</p>
+                    </div>
+                  </div>
+                )
+              })
           })}
         </div>
         <div className="input-pane flex">
-          <textarea className="user-input" value={content} onChange={handleContentChange} />
-          <button className="btn font-size-4" onClick={submitMessages}>Enter</button>
+          <textarea 
+            className="user-input" 
+            placeholder="Please type out your questions..."
+            value={content} 
+            onChange={handleContentChange} />
+          <div className="btn font-size-4" onClick={submitMessages}>Enter</div>
         </div>
       </div>
     </div>
